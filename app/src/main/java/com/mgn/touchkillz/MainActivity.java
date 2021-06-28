76900eb938f1e0944ab14fb797eaddb7c5384839
MDMGN <michaelmdvrhack@gmail.com>
2021-06-28 20:31:04 +0200

    initial commit

--- app/src/main/java/com/mgn/touchkillz/MainActivity.java
+++ app/src/main/java/com/mgn/touchkillz/MainActivity.java
@@ -3,9 +3,11 @@ package com.mgn.touchkillz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnLogin;
+    Button btnSingin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
