dd75dd170301b17d2324640911fe7c03f13526c8
MDMGN <michaelmdvrhack@gmail.com>
2021-06-28 20:43:12 +0200

    Register

--- app/src/main/java/com/mgn/touchkillz/MainActivity.java
+++ app/src/main/java/com/mgn/touchkillz/MainActivity.java
@@ -2,8 +2,11 @@ package com.mgn.touchkillz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button btnLogin;
@@ -12,5 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnLogin=findViewById(R.id.btnLogin);
+        btnSingin=findViewById(R.id.btnSingin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Inicia sesión",Toast.LENGTH_LONG).show();
+            }
+        });
+        btnSingin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
