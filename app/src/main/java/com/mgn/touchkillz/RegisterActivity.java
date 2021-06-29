9e678f72e9eba664100a9c530777a5ecb170bb21
MDMGN <michaelmdvrhack@gmail.com>
2021-06-29 20:51:57 +0200

    Working Register

--- app/src/main/java/com/mgn/touchkillz/RegisterActivity.java
+++ app/src/main/java/com/mgn/touchkillz/RegisterActivity.java
@@ -3,12 +3,102 @@ package com.mgn.touchkillz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.PatternMatcher;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.regex.Pattern;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    EditText eTname,eTemail,eTpassword;
+    TextView eTdate;
+    Button btnRegister;
+    ImageView bgz;
+    FirebaseAuth auth; //Firebase Autenticación.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        eTname=findViewById(R.id.name);
+        eTemail=findViewById(R.id.email);
+        eTpassword=findViewById(R.id.password);
+        eTdate=findViewById(R.id.date);
+        btnRegister=findViewById(R.id.btnRegister);
+        bgz=findViewById(R.id.bgz);
+        auth=FirebaseAuth.getInstance();
+        //Obteniendo valor para la fecha.
+        Date date=new Date();
+        SimpleDateFormat dateformat=new SimpleDateFormat("d 'de' MMMM 'del' yyyy");
+        String fecha=dateformat.format(date);
+        eTdate.setText(fecha);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name=eTname.getText().toString();
+                name=name.trim();
+                String email=eTemail.getText().toString();
+                String password=eTpassword.getText().toString();
+                if(name.length()==0 || name.length()<2){
+                    eTname.setError("3 carácteres como mínimo.");
+                    eTname.setFocusable(true);
+                }else if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    eTemail.setError("Correo electrónico no válido");
+                    eTemail.setFocusable(true);
+                }else if(password.length()<6){
+                    eTpassword.setError("6 carácteres como mínimo.");
+                    eTpassword.setFocusable(true);
+                }else{
+                    PlayerRegister();
+                }
+            }
+        });
+        changeImagen();
+    }
+
+    private void PlayerRegister() {
+        Toast.makeText(RegisterActivity.this,"¡Registro realizado con éxito!.",Toast.LENGTH_SHORT).show();
+    }
+    private void radomImagen() {
+        String bgImages[] = {"bgz0", "bgz1"};
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+
+                    }
+                }, 1000);
+    }
+    private void changeImagen(){
+        String bgImages[] = {"bgz0", "bgz1","bgz2","bgz3"};
+        Handler handler=new Handler();
+        Runnable run=new Runnable() {
+            @Override
+            public void run() {
+                int nrandom = (int) Math.floor(Math.random() * bgImages.length);
+                int idImage = RegisterActivity.this.getResources().getIdentifier(bgImages[nrandom], "drawable", RegisterActivity.this.getPackageName());
+                AlphaAnimation animation1 = new AlphaAnimation(0.5f, 1.0f); //here is a bit of animation for ya ;)
+                animation1.setDuration(1000);
+                animation1.setStartOffset(300); //time for that color effect
+                animation1.setFillAfter(true);
+                bgz.startAnimation(animation1);
+                bgz.setImageResource(idImage);
+                handler.postDelayed(this,1000);
+            }
+        };
+        handler.postDelayed(run,1000);
     }
 }
