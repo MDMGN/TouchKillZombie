ff7d15d2ae0af902095d90b580326c14b2fc8234
MDMGN <michaelmdvrhack@gmail.com>
2021-06-30 16:55:33 +0200

    HandZombie Imagen MainActivity animation.

--- app/src/main/java/com/mgn/touchkillz/MainActivity.java
+++ app/src/main/java/com/mgn/touchkillz/MainActivity.java
@@ -4,19 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    Button btnLogin;
-    Button btnSingin;
+    Button btnLogin,btnSingin;
+    ImageView handzombie;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnLogin=findViewById(R.id.btnLogin);
         btnSingin=findViewById(R.id.btnSingin);
+        handzombie=findViewById(R.id.handzImagen);
+        animationHandZombie(handzombie);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -30,5 +35,21 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+    private void animationHandZombie(ImageView imagen){
+        Handler handler=new Handler();
+        Runnable run=new Runnable() {
+            @Override
+            public void run() {
+                AlphaAnimation animation1 = new AlphaAnimation(0.5f, 1.0f); //here is a bit of animation for ya ;)
+                animation1.setDuration(500);
+                animation1.setStartOffset(200); //time for that color effect
+                animation1.setFillAfter(true);
+                imagen.startAnimation(animation1);
+                imagen.setImageResource(R.drawable.hand_zombie);
+                handler.postDelayed(this,1000);
+            }
+        };
+        handler.postDelayed(run,1000);
     }
 }
