960e3f404912784447bdaf21f5fd3f948e7b9e2b
MDMGN <michaelmdvrhack@gmail.com>
2021-06-30 21:29:16 +0200

    Menú working

--- app/src/main/java/com/mgn/touchkillz/MenuActivity.java
+++ app/src/main/java/com/mgn/touchkillz/MenuActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -14,14 +15,28 @@ import com.google.firebase.auth.FirebaseUser;
 public class MenuActivity extends AppCompatActivity {
     FirebaseAuth auth;
     FirebaseUser user;
-    Button btnSingOut;
+    Button btnSingOut,btnPlay,btnRecordall,btnInfo;
+    TextView nameUser,emailUser,recordUser,uidUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
         auth=FirebaseAuth.getInstance();
         user=auth.getCurrentUser();
+        nameUser=findViewById(R.id.nameUser);
+        emailUser=findViewById(R.id.emailUser);
+        recordUser=findViewById(R.id.recorUser);
+        uidUser=findViewById(R.id.uidUser);
+        btnPlay=findViewById(R.id.btnPlay);
+        btnRecordall=findViewById(R.id.btnRecordall);
+        btnInfo=findViewById(R.id.btnInfo);
         btnSingOut=findViewById(R.id.btnSingOut);
+       btnPlay.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Toast.makeText(MenuActivity.this, "Jugar", Toast.LENGTH_SHORT).show();
+           }
+       });
         btnSingOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -43,7 +58,8 @@ public class MenuActivity extends AppCompatActivity {
         Toast.makeText(MenuActivity.this, "Cerrastes sessión.", Toast.LENGTH_SHORT).show();
         finish();
     }
-
+    public void setDataUser(){
+    }
     @Override
     protected void onStart() {
         isUserLogin();
