67c83b8c65b731c897030e1f779341ac87292e24
MDMGN <michaelmdvrhack@gmail.com>
2021-07-01 16:45:11 +0200

    Finish menu and fix hash md5.

--- app/src/main/java/com/mgn/touchkillz/LoginActivity.java
+++ app/src/main/java/com/mgn/touchkillz/LoginActivity.java
@@ -68,34 +68,4 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
-    public String MD5(String md5) {
-        try {
-            java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
-            byte[] array = md.digest(md5.getBytes());
-            StringBuffer sb = new StringBuffer();
-            for (int i = 0; i < array.length; ++i) {
-                sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1,3));
-            }
-            return sb.toString();
-        } catch (java.security.NoSuchAlgorithmException e) {
-        }
-        return null;
-    }
-    /*public static String sha256(String base) {
-        try{
-            MessageDigest digest = MessageDigest.getInstance("SHA-256");
-            byte[] hash = digest.digest(base.getBytes("UTF-8"));
-            StringBuffer hexString = new StringBuffer();
-
-            for (int i = 0; i < hash.length; i++) {
-                String hex = Integer.toHexString(0xff & hash[i]);
-                if(hex.length() == 1) hexString.append('0');
-                hexString.append(hex);
-            }
-
-            return hexString.toString();
-        } catch(Exception ex){
-            throw new RuntimeException(ex);
-        }
-    }*/
 }
