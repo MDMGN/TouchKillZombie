a7623b1de29f5ee5c2e5ca90c503acc66bba9f22
MDMGN <michaelmdvrhack@gmail.com>
2021-07-06 16:51:01 +0200

    Working Level GameActivity.

--- app/src/main/java/com/mgn/touchkillz/GameActivity.java
+++ app/src/main/java/com/mgn/touchkillz/GameActivity.java
@@ -1,18 +1,17 @@
 package com.mgn.touchkillz;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.LinearLayoutCompat;
 
 import android.app.Dialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.Point;
 import android.graphics.Typeface;
-import android.media.Image;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
+import android.util.Log;
 import android.view.Display;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
@@ -25,10 +24,14 @@ import java.util.Random;
 public class GameActivity extends AppCompatActivity {
     String name,points;
     TextView pointsUser,nameUser,tVtime,tVheight,tVwidth;
-    ImageView pjzombie;
+    ImageView pj;
+    LinearLayoutCompat lnLayout;
+    int lnLayoutHeight;
     int widthScreen;
     int heightScreen;
     int count;
+    int timeMili=1000;
+    boolean pclick=false,click=true;
     Random random;
     boolean gameOver=false;
     Dialog dialog;
@@ -39,7 +42,8 @@ public class GameActivity extends AppCompatActivity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         tVheight=findViewById(R.id.heightscreen);
         tVwidth=findViewById(R.id.widthscreen);
-        pjzombie=findViewById(R.id.pjzombie);
+        lnLayout=findViewById(R.id.lnLayout);
+        pj=findViewById(R.id.pj);
         dialog=new Dialog(GameActivity.this);
         pointsUser=findViewById(R.id.tVpoint);
         nameUser=findViewById(R.id.tVname);
@@ -49,48 +53,51 @@ public class GameActivity extends AppCompatActivity {
         points=intent.getString("recordpoint");
         pointsUser.setText(points);
        nameUser.setText(name);
+        lnLayout.post(new Runnable(){
+            public void run(){
+                lnLayoutHeight = lnLayout.getHeight();
+            }
+        });
        screen();
        countBack();
-       pjzombie.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
-               if(!gameOver) {
-                       count++;
-                       pointsUser.setText(String.valueOf(count));
-                       pjzombie.setImageResource(R.drawable.zombieaplastado);
-                       AlphaAnimation animation1 = new AlphaAnimation(0.5f, 1.0f); //here is a bit of animation for ya ;)
-                       animation1.setDuration(500);
-                       animation1.setStartOffset(300); //time for that color effect
-                       pjzombie.startAnimation(animation1);
-                       new Handler().postDelayed(new Runnable() {
-                           @Override
-                           public void run() {
-                               pjzombie.setImageResource(R.drawable.zombie);
-                               moverZombie();
-                           }
-                       }, 100);
+       fastMoveZombie();
+
+      pj.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+              if(!gameOver) {
+                   if(!pclick) {
+                        actionPJ("pjzombie");
+                       pclick=true;
+                   }else{
+                       click=true;
+                       String pjimagen=v.getTag().toString();
+                       actionPJ(pjimagen);
                    }
                }
-           });
+           }
+       });
     }
     private void screen(){
         Display display=getWindowManager().getDefaultDisplay();
-        Point poitn=new Point();
-        display.getSize(poitn);
-        widthScreen=poitn.x;
-        heightScreen=poitn.y;
+        Point point=new Point();
+        display.getSize(point);
+        widthScreen=point.x;
+        heightScreen=point.y;
         tVwidth.setText(String.valueOf(widthScreen));
         tVheight.setText(String.valueOf(heightScreen));
         random=new Random();
     }
     private void moverZombie(){
         int min=0;
-        int maxX=widthScreen-pjzombie.getWidth();
-        int maxY=heightScreen-pjzombie.getHeight();
+        int maxX=widthScreen-pj.getWidth();
+        int maxY=(heightScreen-lnLayoutHeight)-pj.getHeight();
         int randomX=random.nextInt(((maxX-min)+1)+min);
         int randomY=random.nextInt(((maxY-min)+1)+min);
-        pjzombie.setTranslationX(randomX);
-        pjzombie.setY(randomY);
+        Log.d("randomX",randomX+"");
+        Log.d("randomY",randomY+"");
+        pj.setX(randomX);
+        pj.setY(randomY);
     }
     private void countBack(){
         new CountDownTimer(10000,1000){
@@ -154,24 +161,54 @@ public class GameActivity extends AppCompatActivity {
             }
         });
     }
-    public void randomPJ(){
-        String bgImages[] = {"pjZombie", "pjHuman"};
-        Handler handler=new Handler();
-        Runnable run=new Runnable() {
-            @Override
-            public void run() {
-                int nrandom = (int) Math.floor(Math.random() * bgImages.length);
-                switch (bgImages[nrandom]){
-                    case "pjZombie":
-                        pjzombie=findViewById(R.id.pjzombie);
-                        break;
-                    case "pjHuman":
-                        pjzombie=findViewById(R.id.pjhuman);
-                }
-                handler.postDelayed(this,1000);
-            }
-        };
-        handler.postDelayed(run,1000);
+    public String randomPJ(){
+        String pjImages[] = {"pjzombie","pjzombie","pjzombiecoco","pjhuman", "pjzombie"};
+        int nrandom = (int) Math.floor(Math.random() * pjImages.length);
+        return pjImages[nrandom];
+    }
+    private void actionPJ(String pjImage){
+        int idImageKill = GameActivity.this.getResources().getIdentifier(pjImage+"_kill", "drawable",GameActivity.this.getPackageName());
+        pj.setImageResource(idImageKill);
+        AlphaAnimation animation = new AlphaAnimation(0.5f, 1.0f); //here is a bit of animation for ya ;)
+        animation.setDuration(500);
+        animation.setStartOffset(300); //time for that color effect
+        animation.setFillAfter(true);
+        pj.startAnimation(animation);
+        switch(pjImage){
+            case "pjhuman":
+                count=0;
+                gameOver=true;
+                tVtime.setText(R.string.time_game);
+                mensaggeGameOver();
+                break;
+            default:
+                count++;
+                click=false;
+                break;
+        }
+        pointsUser.setText(String.valueOf(count));
+    }
+    private void fastMoveZombie() {
+        if(!gameOver) {
+            Handler handler = new Handler();
+            Runnable run = new Runnable() {
+                @Override
+                public void run() {
+                        if(!gameOver) {
+                                moverZombie();
+                            if(!click) {
+                                String pjimagen=randomPJ();
+                                   int idImage = GameActivity.this.getResources().getIdentifier(pjimagen, "drawable", GameActivity.this.getPackageName());
+                                    pj.setImageResource(idImage);
+                                    pj.setTag(pjimagen);
+                                    handler.postDelayed(this, timeMili);
+                            }
+                        }
+                    }
+            };
+            handler.postDelayed(run, timeMili);
+            click=false;
+        }
     }
     @Override
     public void onBackPressed() {
